b67bff7d2baa80de6240c8a0e14ee8c6fd1002e6
Carlos Blanco <carlosblanco@me.com>
2022-10-27 22:00:27 +0200

    Fix canvas drawImage

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = '/images/gb_walk.png';
+playerImage.src = '/images/bernard.png';
+
+console.log(playerImage.src)
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(playerImage, 0, 0);
+  requestAnimationFrame(animate);
+}
+
+animate();
