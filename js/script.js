f42c621f81d7fc8ada44ddeab44bb1aca3a0a271
Carlos Blanco <carlosblanco@me.com>
2022-10-27 22:10:50 +0200

    canvas size

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 300;
+const CANVAS_HEIGHT = canvas.height = 300;
 
 const playerImage = new Image();
 playerImage.src = '/images/bernard.png';
