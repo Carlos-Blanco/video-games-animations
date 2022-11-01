3d012223b985bd3754574af82a2f580c456fff04
Carlos Blanco <carlosblanco@me.com>
2022-11-01 19:31:41 +0100

    Slowing animation

--- js/script.js
+++ js/script.js
@@ -12,12 +12,18 @@ const spriteHeight = 156;
 let frameX = 5;
 let frameY = 0;
 
+let gameFrame = 0;
+const staggerFrame = 7;
+
 console.log(playerImage.src)
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  if (frameX < 6) frameX++
-  else frameX=0;
+  if(gameFrame % staggerFrame == 0) {
+    if (frameX < 5) frameX++;
+    else frameX = 0;
+  }
+  gameFrame++
   requestAnimationFrame(animate);
 }
 
